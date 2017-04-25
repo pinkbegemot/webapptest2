91e58d993b2706f33c2a15dd26c2b1e2ad91eb13
pinkbegemot <hipporosa@gmail.com>
2017-04-25 14:04:45 +0100

    Add files via upload
    The PhoneBookTest app modified using Entity Framework's  DbContext class (PhoneBookContext). The original class DatabaseUtils is not used in this solution, sorry.

---
+++ Person.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhoneBookTestApp {
+    public class Person {
+        [Key]
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ PhoneBookContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace PhoneBookTestApp {
+    internal class PhoneBookContext : DbContext {
+        public PhoneBookContext()
+            : base("PhonebookConn") {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<PhoneBookContext>());
+            Database.SetInitializer(new DropCreateDatabaseAlways<PhoneBookContext>());
+        }
+
+        public virtual DbSet<Person> Phonebook { get; set; }
+    }
+}
